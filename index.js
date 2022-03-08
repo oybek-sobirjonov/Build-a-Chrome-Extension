69b24a253243f1f01c4a15b86ed374105a2f7f98
oybek-sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2022-03-08 22:28:45 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const myLeads = [];
+const inputEl = document.querySelector("#input-el");
+
+const inputBtn = document.querySelector("#input-btn");
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked")
+});
