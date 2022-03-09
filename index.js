e849645cd57c8bd60a972e629dfc536eed480c3a
oybek-sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2022-03-09 07:41:59 +0500

    5:29:49
    5:29:49 gacha done

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const myLeads = [];
 const inputEl = document.querySelector("#input-el");
-
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked")
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
 });
+
+function renderLeads() {
+    let listItems = "";
+    for(let i = 0; i < myLeads.length; i++) {
+        listItems += "<li><a href =" + myLeads[i] + " target = '_blank'>" + myLeads[i] + "</a></li>";
+    }
+    ulEl.innerHTML += listItems;
+}
