06d7c8031b06565164a3bf6e1b1a4f100f50ef8e
oybek-sobirjonov <87658070+oybek-sobirjonov@users.noreply.github.com>
2022-03-09 09:21:40 +0500

    6:13:00
    6:13:00 gacha done

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+localStorage.clear();
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) );
     renderLeads();
 });
 
 function renderLeads() {
     let listItems = "";
     for(let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a href =" + myLeads[i] + " target = '_blank'>" + myLeads[i] + "</a></li>";
+        listItems += `
+            <li>
+                <a href ='${myLeads[i]}' target = '_blank'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
     }
-    ulEl.innerHTML += listItems;
+    ulEl.innerHTML = listItems;
 }
